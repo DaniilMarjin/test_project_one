f85c8e25668ea8f9deb2a34201e383ea89fb671c
Даниил Марьин <d_marjin@mail.ru>
2022-06-15 10:59:48 +0300

    add project splaech screen'

---
+++ Gif_splash_screen/Gif_splash_screen/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Gif_splash_screen
+{
+    
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer dt = new DispatcherTimer();
+        public MainWindow()
+        {
+            InitializeComponent();
+            dt.Tick += new EventHandler(dt_Tick);
+            dt.Interval = new TimeSpan(0, 0, 3);
+            dt.Start();
+        }
+        private void dt_Tick(object sender, EventArgs e)
+        {
+            DashBoard db = new DashBoard();
+            db.Show();
+
+            dt.Stop();
+            this.Close();
+        }
+    }
+}
---
+++ Gif_splash_screen/Gif_splash_screen/MainWinodw.xaml.vb
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gif_splash_screen
+{
+    class MainWinodw
+    {
+        Sub New()
+            InitializeComponent()
+            Dim Gif_progect As Gif_splash_screen()
+            Me.AddChild(Gif_progect)
+        End Sub
+    }
+}
